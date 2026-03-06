17b375c78242963c4164a55b9f2cd26e291115a1
Rdlodha <rdlodha007@gmail.com>
2026-03-06 16:51:47 +0530

    fix: force IPv4 for nodemailer to fix Render timeouts

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,7 +6,9 @@ const nodemailer = require("nodemailer");
  */
 async function sendEmail({ to, subject, text, html }) {
     const transporter = nodemailer.createTransport({
-        service: process.env.EMAIL_SERVICE || "gmail",
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // use SSL
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
